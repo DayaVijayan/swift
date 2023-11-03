5be02725e973cfb318978054c63b9cd3f6ae0aeb
DayaVijayan <149620121+DayaVijayan@users.noreply.github.com>
2023-11-03 19:01:24 +0530

    Add files via upload

---
+++ TicTacToeSwiftUI/TicTacToeSwiftUI/TicTacToeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeSwiftUIApp.swift
+//  TicTacToeSwiftUI
+//
+//  Created by daya vijayan on 03/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TicTacToeSwiftUI/TicTacToeSwiftUI/cell.swift
@@ -0,0 +1,40 @@
+//
+//  cell.swift
+//  TicTacToeSwiftUI
+//
+//  Created by daya vijayan on 03/11/23.
+//
+import SwiftUI
+import Foundation
+struct Cell{
+    var tile: Tile
+    
+    func displayTile() -> String
+    {
+        switch (tile){
+        case Tile.Nought:
+            return "0"
+        case Tile.Cross:
+            return "X"
+        default:
+            return ""
+        }
+    }
+    func tileColor() -> Color{
+        switch(tile){
+        case Tile.Nought:
+            return Color.red
+        
+        case Tile.Cross:
+        return Color.black
+        default:
+            return Color.black
+    }
+    }
+    
+}
+enum Tile{
+    case Nought
+    case Cross
+    case Empty
+}
